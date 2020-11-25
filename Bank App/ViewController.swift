b74d08ef4faa7a5f48b0dc88f215e33083d286cd
Djamal <Trim_D_J@mail.ru>
2020-11-25 19:35:45 +0300

    Add Tab Bar

--- Bank App/ViewController.swift
+++ Bank App/ViewController.swift
@@ -9,9 +9,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountLabel: UILabel?
+    
+    var amount = 5687.20{
+        didSet {
+            updateAmountLabel()
+        }
+    }
+    
+    func updateAmountLabel() {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "$"
+        let number = NSNumber(value: amount)
+        amountLabel?.text = formatter.string(from: number)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        updateAmountLabel()
     }
 
 
